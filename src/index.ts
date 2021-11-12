0c9a5bc54bf1621b6e12bed9e574f10dc85c6238
Khaled El Hamzi <contact@elhamzi.me>
2021-11-12 02:07:29 +0100

    Subscribe to the store.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import store from './store'
 
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed!", store.getState());
+})
+
 store.dispatch({
     type: "ADD_BUG",
     payload: {
         description: "Bug-001"
     }
 })
+
+unsubscribe();
 
 store.dispatch({
     type: "REMOVE_BUG",
