6efe06b43f22ec01f395cdcb31a5dfb2683ac696
Khaled El Hamzi <contact@elhamzi.me>
2025-01-23 12:07:37 +0100

    core: remove the old implementation and generate react/redux  using vite.

--- src/store/apiActions.ts
+++
@@ -1,7 +0,0 @@
-import { ActionCreatorWithPayload, createAction,  } from "@reduxjs/toolkit"
-
-export const apiCallBegan:ActionCreatorWithPayload<any> = createAction("api/callBegan");
-export const apiCallSuccess: ActionCreatorWithPayload<any> = createAction("api/callSuccess");
-export const apiCallFailed: ActionCreatorWithPayload<any> = createAction("api/callFailed");
-
- 
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Optional: Adds a shorthand alias for importing from the src directory
+    },
+  },
+  server: {
+    open: true, // Automatically opens the app in the browser when the dev server starts
+  },
+  build: {
+    outDir: 'dist', // Output directory for production builds
+    assetsDir: 'assets', // Directory for static assets in production
+  },
+})
