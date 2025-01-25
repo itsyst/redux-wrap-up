c296f1f24fe2f6acac8f716df99435190bb1c277
Khaled El Hamzi <contact@elhamzi.me>
2025-01-25 14:20:40 +0100

    core: remove unused `entities`, `uis` and `components`

--- src/store/middleware/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Action, Dispatch } from "@reduxjs/toolkit";
-
-const logger = (param: unknown) => (_store: unknown) => (next: Dispatch) => (action: Action) => {
-    console.log("Logging", param, _store)
-    next(action)
-}
-
-export default logger;
