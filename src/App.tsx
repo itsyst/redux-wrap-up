da4fd6434abbb9ff29eb9b199b09069e9cc3de62
Khaled El Hamzi <contact@elhamzi.me>
2025-01-25 23:06:29 +0100

    feat: add dark mode toggle and improve UI layout
    - Added a dark mode toggle feature using `useState` to manage the theme state.
    - Integrated `LuMoon` and `LuSun` icons from `react-icons/lu` for the toggle button.
    - Updated the component structure and styling to support dark mode and improve layout.
    - Enhanced the header layout with a flexbox for better alignment of the title and toggle button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { getBugs } from './store/entities/bugs';
-import store from './store/store';
-import BugAdd from './components/bugAdd';
 import BugList from './components/bugList';
+import { getBugs } from './store/entities/bugs';
 import { getUsers } from './store/entities/users';
+import store from './store/store';
+import { LuMoon, LuSun } from 'react-icons/lu';
 
 function App() {
 	const dispatch = useDispatch();
+	const [isDarkMode, setIsDarkMode] = useState(false);
 
+	const toggleDarkMode = () => {
+		setIsDarkMode(!isDarkMode);
+	};
 	useEffect(() => {
 		store.dispatch(getBugs());
 		store.dispatch(getUsers());
 	}, [dispatch]);
 
 	return (
-		<div className="mt-4">
-			<h1 className="text-uppercase mb-3">Bug Tracker</h1>
-			<BugAdd />
-			<BugList />
+		<div className={isDarkMode ? 'app-container dark-mode' : 'app-container'}>
+			<div className="header d-flex justify-content-between align-items-center">
+				<h1 className="text-uppercase mb-3">Bug Tracker</h1>
+				<button
+					onClick={toggleDarkMode}
+					className={isDarkMode ? 'btn btn-light' : 'btn btn-dark'}
+				>
+					{isDarkMode ? <LuSun /> : <LuMoon />}
+				</button>
+			</div>
+			<div className="bug-list-container">
+				<BugList />
+			</div>
 		</div>
 	);
 }
