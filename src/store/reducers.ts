4aa7217955747be0bc04765f927990ea61919312
Khaled El Hamzi <contact@elhamzi.me>
2025-01-25 14:29:19 +0100

    refactor: simplify root reducer by removing projects, loading, modal, and notifications reducers

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,21 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import bugsReducer from './entities/bugs';
-import projectsReducer from './entities/projects';
 import usersReducer from './entities/users';
-import loadingReducer from "./ui/loading";
-import notificationsReducer from "./ui/notifications";
-import modalReducer from "./ui/modal";
 
 export default combineReducers({
     entities: combineReducers({
         bugs: bugsReducer,
-        projects: projectsReducer,
         users: usersReducer
-    }),
-    ui: combineReducers({
-        loading: loadingReducer,
-        modal: modalReducer,
-        notifications: notificationsReducer
     })
 })
 
